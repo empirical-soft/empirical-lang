71beeba760ba4f021d84e3ead2172da07ffd3ce0
Christopher C. Aycock <christopher.aycock@empirical-soft.com>
2019-05-30 20:46:23 -0400

    Added deployments to AppVeyor

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -2,4 +2,4 @@ rem Run this script after a build to copy the binary to the web repo
 
 cd build\MinSizeRel
 powershell.exe -nologo -noprofile -command "& { Compress-Archive -Path empirical.exe -DestinationPath deploy.zip }"
-move /y deploy.zip ..\..\..\website\windows.zip
+move /y deploy.zip ..\..\windows.zip
